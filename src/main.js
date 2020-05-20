c6caece2a60123575ee35185d1cce383b39f0c41
qq540618173 <540618173@qq.com>
2020-05-20 14:59:26 +0800

    接口错误拦截

--- src/main.js
+++ src/main.js
@@ -1,10 +1,41 @@
 import Vue from 'vue'
-import App from './App.vue'
 import router from './router'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+import App from './App.vue'
+
+//请求地址
+axios.defaults.baseURL = '/api'
+//超时时间
+axios.defaults.timeout = 8000
+
+/**
+ * 请求拦截
+ */
+axios.interceptors.request.use(() => {
+
+})
+
+/**
+ * 响应拦截
+ */
+axios.interceptors.response.use((response) => {
+	let res = response.data
+	if(res.data === 0){
+		return res.data
+	} else if(res.status === 10){
+		//未登录
+		window.location.href = '/#/login'
+	} else {
+		alert(res.msg)
+	}
+})
+
+Vue.use(VueAxios, axios)
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App),
+	router,
+	render: h => h(App),
 }).$mount('#app')
