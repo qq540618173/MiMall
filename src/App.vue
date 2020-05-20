5749976cad66a8f1178dd4befbe96e6dc2529d51
qq540618173 <540618173@qq.com>
2020-05-20 14:29:25 +0800

    封装storage

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,29 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-
+import storage from './storage'
 
 export default {
-  name: 'App',
-  components: {
-    
-  }
+	name: 'App',
+	components: {
+
+	},
+	data() {
+		return{
+
+		}
+	},
+	mounted(){
+		// storage.setItem('avc', {"firstName":'wang'}, 'user')
+		storage.clear('sex', 'user')
+	}
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
