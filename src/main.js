49eacf4eae0d7486df53b20ca53deb11ba74e3e9
qq540618173 <540618173@qq.com>
2020-05-21 10:13:05 +0800

    环境配置

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@ import Vue from 'vue'
 import router from './router'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import env from './env'
 
 import App from './App.vue'
 
-//请求地址
-axios.defaults.baseURL = '/api'
-//超时时间
+// 根据环境变量获取不同的请求地址
+// axios.defaults.baseURL = '/api'
+axios.defaults.baseURL = env.baseURL
+// 超时时间
 axios.defaults.timeout = 8000
 
 /**
