b37dfaf7b25bc80acf49e8c77b9cdba74caf3332
qq540618173 <540618173@qq.com>
2020-05-21 17:43:37 +0800

    NavHeader组件开发，引入公共样式和图片

--- src/components/NavHeader.vue
+++ src/components/NavHeader.vue
@@ -1,5 +1,22 @@
 <template>
-    <div>nav-header</div>
+    <div class="hearder">
+        <div class="nav-topbar">
+            <div class="container">
+                <div class="topbar-menu">
+                    <a href="javascript:;">小米商城</a>
+                    <a href="javascript:;">MUI</a>
+                    <a href="javascript:;">云服务</a>
+                    <a href="javascript:;">协议规则</a>
+                </div>
+                <div class="topbar-user">
+                    <a href="javascript:;">登录</a>
+                    <a href="javascript:;">注册</a>
+                    <a class="my-cart" href="javascript:;"><span class="icon-cart"></span>购物车</a>
+                </div>
+            </div>
+        </div>
+        <div class="nav-header"></div>
+    </div>
 </template>
 
 <script>
@@ -8,6 +25,44 @@ export default {
 }
 </script>
 
-<style lang="stylus">
+<style lang="scss">
+.hearder{
+    .nav-topbar{
+        height: 39px;
+        line-height: 39px;
+        background-color: #333333;
+        color: #b0b0b0;
 
+        .container{
+            width: 1226px;
+            margin: 0 auto;
+            display: flex;
+            justify-content: space-between;
+            align-content: center;
+
+            a{
+                display: inline-block;
+                color: #b0b0b0;
+                margin-right: 17px;
+            }
+
+            .my-cart{
+                width: 110px;
+                background-color: #ff6600;
+                text-align: center;
+                color: #ffffff;
+
+                .icon-cart{
+                    display: inline-block;
+                    width: 16px;
+                    height: 12px;
+                    margin-right: 4px;
+                    background: url(./../assets/imgs/icon-cart.png) center no-repeat;
+                    background-size: contain;
+                }
+            }
+        }
+    }
+    
+}
 </style>
